cb13dd843229aca635a0fcb22027797aa9f8e710
Kenneth ONeal <12keniaoneal21@gmail.com>
2023-10-12 21:51:24 -0600

    10/12/23 I hate tic tac toe but I finished it - Ken

--- TheCoolestTicTacToe/Program.cs
+++ TheCoolestTicTacToe/Program.cs
@@ -1,47 +1,79 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Dynamic;
+﻿using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
 
-Console.WriteLine("Hello, World!");
+// 2 players, turn based
 
-string myStringThing;
-myStringThing = "null";
+// prompt-based inputs from each player
+// display board
+// update board per turn
 
-Console.WriteLine(myStringThing);
+// check for win
+// check for draw
+// is move valid?
+// win, lose and draw states
 
-string firstNumber = "10";
-string secondNumber = "2";
+// start game
+// reset game
+// give up.
 
-Console.WriteLine(int.Parse(firstNumber) + int.Parse(secondNumber));
+// board class -> displayBoard(), placeMark(Player player), clearBoard(), validateSpace()
+// player class -> endTurn(), checkForWin(), checkForDraw()
+//this doesnt seem right cause the player cant check the board aspects
+// hmmmmmmmmmm
 
-Jeff jeffOry = new Jeff(2, 6);
-
-Console.WriteLine(jeffOry.addStuff(firstNumber, secondNumber));
-Console.WriteLine(jeffOry.addStuff());
-
-public class Jeff
+/*
+public class Game
 {
-private short shortOne {get; set;} //adds default getter/setters thank you C#
-private short shortTwo {get; set;}
+    private int[] board = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+    private int totalTurns = 0; //++ every round, if == 9 game over // dont check until turn 5
+    private int player = 1; //player numbers self explanitory, make 1 = x and 2 = o
+    
+    //kenneth, we need to keep track of spots on a 3x3 grid, could use a multidimensional array but it sucks ass
+    //we also need to differenciate players
+    //calling this game seems better to me fr fr
 
-public Jeff() {}
-public Jeff(short s1, short s2) 
-{
-    shortOne = s1;
-    shortTwo = s2;
-}
+    //{" | | |", " | | | ", " | | | "}; //board display
 
-public int addStuff()
-{
-    return shortOne + shortTwo;
-}
-public int addStuff(int numberOne, int numberTwo)
-{
-    return numberOne + numberTwo;
-}
 
-public string addStuff(string numberOne, string numberTwo) {
-    var addedInt = int.Parse(numberOne) + int.Parse(numberTwo);
-    return addedInt.ToString();
-}
+    public void Display()
+    {
+        //show the board loser
+        //literally the easiest part but NOOOO 
+
+        // provide options 1-9
+        // loop over until given valid answer
+        // whatever the player chooses, it puts their mark on that spot
+        // repeat while removing taken spaces
+    }
+
+    public void Clear()
+    {
+        //set all symbols to spaces
+    }
+
+    public bool IsValidPlacement()
+    {
+        //if space != 1 or 2, valid
+
+        return true;
+    }
+
+    public void PlaceMark() //parameters including mark n player
+    {
+        //validate legal placement of mark
+        //player 1 = x, player 2 = o, replace the numbers with characters
+
+        //update board space to show mark
+    }
+
+    public int CheckForWin()
+    {
+        //nested for loops perhaps?
+        //if space 1 = x and space 2 = x and space 3 = x, player 1 wins
+        //repeat until you die
+
+        return 0;
+    }
 
 }
+*/
